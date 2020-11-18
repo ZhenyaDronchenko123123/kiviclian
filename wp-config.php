beb8babfe23b562a7873dd430e390117ea3d13cf
Zhenya <55761204+ZhenyaDronchenko123123@users.noreply.github.com>
2020-11-18 10:10:17 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,14 +20,14 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u271619410_kiviclean' );
+define( 'DB_NAME', '' );
 
 
 /** MySQL database username */
-define( 'DB_USER', 'u271619410_kiviclean' );
+define( 'DB_USER', '' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Barbers1!u271619410_' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
